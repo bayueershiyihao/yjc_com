f50a8102467ad840a18e55f567bc7b84f528df98
bayueershiyihao <76773766+bayueershiyihao@users.noreply.github.com>
2021-10-11 22:01:03 +0900

    Finish

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+const login = document.querySelector(".login");
+const loginPopup2 = document.querySelector(".login-popup");
+const menuBtn = document.querySelector(".menu-toggle_btn");
+const menu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu_show");
+
+function loginPopup(e) {
+  const loginContainer = document.querySelector(".login-container");
+  loginContainer.classList.toggle("display-none");
+}
+
 function toggleBtnMenu() {
-  const menu = document.querySelector(".menu");
-  const menuBtn = document.querySelector(".menu-toggle_btn");
   let widthSize = window.innerWidth;
   if (widthSize < 600) {
-    menu.classList.add("toggle-btn");
-    menuBtn.classList.remove("toggle-btn");
+    menu.classList.add("display-none");
+    menuBtn.classList.remove("display-none");
   } else {
-    menu.classList.remove("toggle-btn");
-    menuBtn.classList.add("toggle-btn");
+    menu.classList.remove("display-none");
+    menuBtn.classList.add("display-none");
   }
 }
 
-window.addEventListener("resize", toggleBtnMenu);
+function showToggleMenu() {
+  const mobileClose = document.querySelector(".mobile-close");
+  mobileClose.addEventListener("click", () => {
+    mobileMenu.classList.add("display-none");
+  });
+  mobileMenu.classList.remove("display-none");
+}
+
+function init() {
+  menuBtn.addEventListener("click", showToggleMenu);
+  window.addEventListener("resize", toggleBtnMenu);
+  window.addEventListener("load", toggleBtnMenu);
+  login.addEventListener("click", loginPopup);
+}
+
+init();
